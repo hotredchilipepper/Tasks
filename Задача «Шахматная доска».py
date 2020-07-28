1cd25c2d59149373f2d0e86b1dcd06c4b2365747
Юрий <68888570+hotredchilipepper@users.noreply.github.com>
2020-07-28 15:25:20 +0700

    Create Задача «Шахматная доска».py

---
+++ Задача «Шахматная доска».py
@@ -0,0 +1,16 @@
+n,m = [int(s) for s in input().split()]
+a = [[0] * m for i in range(n)]
+for i in range(n):
+    for j in range(m):
+        if i % 2 == 0:
+            if j % 2 == 0:
+                a[i][j] = '.'
+            else:
+                a[i][j] = '*'
+        else:
+            if j % 2 == 0:
+                a[i][j] = '*'
+            else:
+                a[i][j] = '.'
+for row in a:
+    print(' '.join(row))
